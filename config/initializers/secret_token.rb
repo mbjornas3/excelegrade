917a246dfbb9d484d05b0a4907b14e5b11d0c434
Nikea Davis <nikeadavis@gmail.com>
2012-04-29 19:16:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Excelegrade::Application.config.secret_token = '406cfc729d37c50a62f86129a4ec7977f8ed2577b5c313a202ea20d514b665377fbf3589163c6f294742aeec3a1f9a4f868c93a044eca8ef30f064fec575021c'
